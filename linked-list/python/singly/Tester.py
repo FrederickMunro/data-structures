383a358cc60eb4bf73584d40c0ee2834a0f58b1d
FrederickMunro <fredmunro7@gmail.com>
2022-11-14 19:00:23 -0500

    Fixed file structure

---
+++ linked-list/python/singly/Tester.py
@@ -0,0 +1,9 @@
+from SinglyLinkedList import SinglyLinkedList
+
+linkedList = SinglyLinkedList(0)
+linkedList.push(1)
+linkedList.push('lemons')
+linkedList.push(3)
+linkedList.push("potatos")
+
+print(repr(linkedList))
